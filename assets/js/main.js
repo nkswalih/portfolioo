6479f78e8a885fcfeda83f41b3da3e8520052402
nkswalih <swalink555@gmail.com>
2025-10-04 23:20:17 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
+/*=============== Cursor ===============*/
+
+window.addEventListener("mousemove", (e)=>{
+    let cursor = document.getElementById("cursor");
+    setTimeout(()=>{
+        cursor.style.top = `${e.clientY}px`;
+        cursor.style.left = `${e.clientX}px`;
+    },50)
+
+    if(e.target.id === "box") {
+        cursor.classList.add("active");
+    }
+    else {
+        cursor.classList.remove("active");
+    }
+})
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
   const header = document.getElementById("header");
@@ -231,3 +247,4 @@ sr.reveal(`.footer, footer__container`, {
   origin: "bottom",
   distance: "30px",
 });
+
